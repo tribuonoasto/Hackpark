cb3c4ce0f932a4458aa5fc18f01fff6f4c39bdee
LordFeexz <anandafiqri1403@gmail.com>
2022-11-02 00:09:41 +0700

    setup

---
+++ servers/user/middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+  let status = 500;
+  let message = "Internal Server Error";
+
+  res.status(status).json({ message });
+};
+
+module.exports = errorHandler;
