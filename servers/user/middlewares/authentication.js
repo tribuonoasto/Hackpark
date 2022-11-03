bdd4a67db88dc96c71c6c1c2bd89902ef9f416d6
LordFeexz <anandafiqri1403@gmail.com>
2022-11-03 08:26:59 +0700

    add authentication

---
+++ servers/user/middlewares/authentication.js
@@ -0,0 +1,29 @@
+const User = require("../models");
+
+const authentication = async (req, res, next) => {
+  try {
+    const { access_token } = req.headers;
+    if (!access_token) throw { name: `invalid token` };
+
+    const payload = verifyToken(access_token);
+
+    const user = await User.findOne({
+      where: {
+        id: payload.id,
+      },
+    });
+    if (!user) throw { name: `invalid token` };
+
+    req.user = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    };
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = authentication;
