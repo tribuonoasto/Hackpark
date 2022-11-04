9cdbb85ebcd1084c891ec27e113095c9f21e2870
LordFeexz <anandafiqri1403@gmail.com>
2022-11-04 19:53:01 +0700

    update vehicle image

--- servers/user/controllers/vehicleController.js
+++ servers/user/controllers/vehicleController.js
@@ -16,6 +16,28 @@ class Controller {
       next(err);
     }
   }
+
+  static async changeImg(req, res, next) {
+    try {
+      const { imgUrl } = req.body;
+
+      const { id } = req.user;
+
+      const vehicle = await Vehicle.findOne({
+        where: {
+          UserId: id,
+        },
+      });
+
+      if (!vehicle) throw { name: "Vehicle not found" };
+
+      await Vehicle.update({ imgUrl }, { where: { UserId: id } });
+
+      res.status(201).json({ message: "Success" });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = Controller;
--- servers/user/routes/vehicleRouter.js
+++ servers/user/routes/vehicleRouter.js
@@ -1,3 +1,9 @@
+const Controller = require("../controllers/vehicleController");
+
 const router = require("express").Router();
+
+router.get("/", Controller.getVehicle);
+
+router.patch("/", Controller.changeImg);
 
 module.exports = router;
