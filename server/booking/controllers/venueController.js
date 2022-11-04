f9abe14f534838d83d70c96a6145f5495833b09a
Tri Buono Asto Nugroho <tribuonoasto@gmail.com>
2022-11-04 07:55:58 +0700

    Add get all venues

--- server/booking/controllers/venueController.js
+++ server/booking/controllers/venueController.js
@@ -1,8 +1,16 @@
 "use strict";
+const Venue = require("../models/venue");
 
 class VenueController {
   static async showAllVenue(req, res, next) {
-    res.send("venue");
+    try {
+      const venues = await Venue.findAll();
+
+      if (!venues || venues.length <= 0) throw { name: "venue_not_found" };
+      res.status(200).json(venues);
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
--- server/booking/models/venue.js
+++ server/booking/models/venue.js
@@ -1,1 +1,18 @@
 "use strict";
+const { getDB } = require("../config/mongo");
+
+class Venue {
+  static async findAll() {
+    try {
+      const collection = getDB().collection("venues");
+
+      const options = {};
+
+      const venues = await collection.find({}, options).toArray();
+      return venues;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+module.exports = Venue;
