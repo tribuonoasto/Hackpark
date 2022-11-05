e4c55ea342c74fd6abd58718db9275511e33568c
LordFeexz <anandafiqri1403@gmail.com>
2022-11-05 17:59:17 +0700

    returning back controller balance

--- servers/user/routes/balanceRouter.js
+++ servers/user/routes/balanceRouter.js
@@ -1,10 +1,7 @@
 const Controller = require("../controllers/balanceController");
-const Index = require("../controllers/midtrans/payments");
 
 const router = require("express").Router();
 
 router.get("/", Controller.getBalance);
-
-router.post("/va/charge", Index.bankTransfer);
 
 module.exports = router;
