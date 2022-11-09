cfe64d4fd2a602471185cb07e93170c0319b51b1
LordFeexz <anandafiqri1403@gmail.com>
2022-11-09 20:09:52 +0700

    setup deploy

--- servers/user/app.js
+++ servers/user/app.js
@@ -1,4 +1,4 @@
-if (process.env.Node_ENV !== "production") require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
